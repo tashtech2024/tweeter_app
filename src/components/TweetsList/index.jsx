1a0eef055c13a46d8b7c4de2ae6fecc14dcf5cfc
Latasha Davis <latashaddavis21@gmail.com>
2024-06-11 09:23:37 -0400

    updated router

--- src/components/TweetsList/index.jsx
+++ src/components/TweetsList/index.jsx
@@ -1,51 +1,90 @@
-import { useState } from "react";
+import { Suspense, useState, useEffect } from "react";
+// import { v4 as uuidv4 } from "uuid";
+// import { ErrorBoundary } from "react-error-boundary";
 import Tweet from "../Tweet";
 import CreateTweetForm from "../CreateTweetForm";
-import { v4 as uuidv4 } from 'uuid';
-import { data } from "../data/data";
+import { data } from "../../data/data";
+import axios from "axios";
 
 import "./TweetsList.module.css";
 
-function TweetsList({}) {
-  const [tweets, setTweets] = useState(data);
+function TweetsList() {
+  const [tweets, setTweets] = useState([]);
 
-  // Create New Tweet
-  const addTweet = (newTweet) => {
-    const tweetDoc = {
-        content: newTweet,
-        username: 'TashUno721',
-        likes: 0,
-        retweets: 0,
-        timestamp: new Date(),
-        id: uuidv4()
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        const res = await axios.get("http://localhost:4000/tweets");
+        console.log(res.data);
+        setTweets([...res.data]);
+      };
+      fetchData();
+    } catch (error) {
+      console.log(error);
     }
+  }, []);
 
-    setTweets([tweetDoc, ...tweets ]);
-  }
-  //* Delete Tweet 
+  const addTweet = async (newTweet) => {
+    // const tweetDoc = {
+    //   content: newTweet,
+    //   username: "abe123",
+    //   likes: 0,
+    //   retweets: 0,
+    //   timestamp: new Date(),
+    //   id: uuidv4(),
+    // };
+
+    // setTweets([tweetDoc, ...tweets]);
+
+    try {
+      const res = await axios.post("http://localhost:4000/tweets", {
+        newTweet,
+        username: "abe123",
+      });
+      console.log(res.data);
+      setTweets([res.data, ...tweets]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const removeTweet = (tweetId) => {
-    setTweets(tweets.filter(t => t.id !== tweetId));
-  }
-  // Update Tweet
+    setTweets(tweets.filter((t) => t._id !== tweetId));
+  };
+
   const updateTweet = (tweetId, newTweetContent) => {
-    setTweets(tweets.map(t => {
-      if (t.id === tweetId) {
+    setTweets(
+      tweets.map((t) => {
+        if (t.id === tweetId) {
           return {
             ...t,
             content: newTweetContent,
-          }
-      } else return t;
-    }));
-  }
+          };
+        } else {
+          return t;
+        }
+      }),
+    );
+  };
+
   return (
     <div className="mt-4">
-      <CreateTweetForm addTweet={addTweet}/>
+      <CreateTweetForm addTweet={addTweet} />
 
+      {/* <ErrorBoundary fallback={<div>Error loading Tweets!</div>}>
+        <Suspense fallback={<div>Loading...</div>}> */}
       <section>
         {tweets.map((item) => (
-          <Tweet tweet={item} key={item.id} removeTweet={removeTweet} updateTweet={updateTweet}/>
+          <Tweet
+            tweet={item}
+            key={item._id ? item._id : item.id}
+            removeTweet={removeTweet}
+            updateTweet={updateTweet}
+          />
         ))}
       </section>
+      {/* </Suspense>
+      </ErrorBoundary> */}
     </div>
   );
 }
